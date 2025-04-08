95184479d199b3994d541916bc296d4a8765ff70
OK946 <owen.kolbert24@student.cart.org>
2025-04-07 19:57:42 -0700

    Fixed the issues with the combat system
    I solved the issues where text wouldn't display properly.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let currentDialogueNumber = 0;
 let storyPathRecord = [];
-let textSpeed = 0;
+let textSpeed = 10;
 const player = {
     health: 3,
     // Companion # meaning
@@ -62,8 +62,8 @@ const firstMiddle = new storyPart(`2a`,[``,`You continue down the middle, but yo
 const secondMiddle = new storyPart(`2b`,[``,`You reach a clearing and are ambushed by a group of hooded figures.`,`Korai pauses for a brief moment before getting frightened, knocking you off his back.`,`As Korai runs off in fear you gather yourself and try to follow your precious horse while avoiding the mysterious men.`,`You lose Korai and, despite your worry, you decide that it would be best to continue to town to get help.`],[{name: "Go to Town", effect: "",}]);
 const thirdMiddle = new storyPart(`2c`,[``,`You go right to avoid the boulder, but are very quickly ambushed by hooded figures hiding in the nearby shrubbery.`,`Korai pauses for a brief moment before getting frightened, knocking you off his back.`,`As Korai runs off in fear you gather yourself and try to follow your precious horse while avoiding the mysterious men.`,`You lose Korai and, despite your worry, you decide that it would be best to continue to town to get help.`],[{name: "Go to Town", effect: "",}]);
 const firstRight = new storyPart(`3a`,[``,`You decide to go right and take in the scenery as Korai strolls towards town.`,`“Rooooooaaaaar.”`,`Your peace is interrupted by a chilling roar and you realize too late that you have come face to face with a monster.`,`The monster attacks. You are knocked off of Korai and scramble to your feet.`,`Korai is startled and begins to thrash around violently. Scared beyond reason, you run.`,`After running for a while you realize that you can’t just leave your best friend behind, but that you have no chance of winning.`,`You can’t decide whether you should go back and fight or run towards town for help. `],[{name: "Go Back for Korai", effect: ()=> Fight(firstFight),}, {name: "Go to Town for Help", effect: ()=> moveTo(secondRight),}]);
-const secondRight = new storyPart(`3b`,[``,`You run towards town as fast as you can.`],[{name: "Reach Town", effect: ()=> moveTo(firstTown),}])
-const firstFightWin = new storyPart(`3c`,[``,`You narrowly win against the monster and reunite with Korai.`,`You ride Korai back to your house and take the long way to the village.`,`As you ride you vow to never venture too deep into the forest and to never try something so frightening ever again.`,`THE END \n However, there are many other ways your story could've played out.`],[{name: "Restart Game", effect: ()=> moveTo(beginning),}]);
+const secondRight = new storyPart(`3b`,[``,`You are cornered by the monster and barely escape with your life.`],[{name: "Reach Town", effect: ()=> moveTo(firstTown),}])
+const firstFightWin = new storyPart(`3c`,[``,`Your attack deals the final blow to the monster`,`You breathe a sigh of relief as you reunite with Korai.`,`You ride Korai back to your house and take the long way to the village.`,`As you ride you vow to never venture too deep into the forest and to never try something so frightening ever again.`,`THE END \n However, there are many other ways your story could've played out.`],[{name: "Restart Game", effect: ()=> moveTo(beginning),}]);
 
 
 const firstTown = new storyPart()
@@ -99,124 +99,128 @@ function Fight(storyFight){
     };
     document.getElementById(`attack`).addEventListener("click", () => {
         if(enemyChoice == 0){
-            textType(document.getElementById("textDisplay"), `You attempted to attack ${storyFight.monster.name}, but they also attacked and both of you took damage`);
             player.health -= 1;
             storyFight.monster.health -= player.attack;
             if(player.health <= 0){
                 fightLose(storyFight)
-                return
-            }
-            if(storyFight.monster.health <= 0){
+                
+            }else if(storyFight.monster.health <= 0){
                 fightWin(storyFight)
-                return
+                
+            } else {
+                textType(document.getElementById("textDisplay"), `You attempted to attack ${storyFight.monster.name}, but they also attacked and both of you took damage`);
+                Fight(storyFight)
             }
-            Fight(storyFight)
         }
         if(enemyChoice == 1){
-            textType(document.getElementById("textDisplay"), `You attempted to attack ${storyFight.monster.name}, but they blocked your attack, rendering it useless.`);
             player.health -= 0.5;
             if(player.health <= 0){
                 fightLose(storyFight)
-                return
-            }
-            if(storyFight.monster.health <= 0){
+                
+            }else if(storyFight.monster.health <= 0){
                 fightWin(storyFight)
-                return
+                
+            } else {
+                textType(document.getElementById("textDisplay"), `You attempted to attack ${storyFight.monster.name}, but they blocked your attack, rendering it useless.`);
+                Fight(storyFight)
             }
-            Fight(storyFight)
         }
         if(enemyChoice == 2){
-            textType(document.getElementById("textDisplay"), `You attempted to attack ${storyFight.monster.name}, and they were caught off guard by the quick attack.`);
             storyFight.monster.health -= player.attack;
             if(player.health <= 0){
                 fightLose(storyFight)
-                return
-            }
-            if(storyFight.monster.health <= 0){
+                
+            }else if(storyFight.monster.health <= 0){
                 fightWin(storyFight)
-                return
+                
+            } else {
+                textType(document.getElementById("textDisplay"), `You attempted to attack ${storyFight.monster.name}, and they were caught off guard by the quick attack.`);
+                Fight(storyFight);
             }
-            Fight(storyFight)
+
         }
         
     });
     document.getElementById(`defend`).addEventListener("click", () => {
         if(enemyChoice == 0){
-            textType(document.getElementById("textDisplay"), `You attempted to defend against ${storyFight.monster.name}'s attack, and it blocked their attack, allowing for a counter attack.`);
             storyFight.monster.health -= (player.attack/2);
             if(player.health <= 0){
                 fightLose(storyFight)
-                return
-            }
-            if(storyFight.monster.health <= 0){
+                
+            }else if(storyFight.monster.health <= 0){
                 fightWin(storyFight)
-                return
+                
+            } else {
+                textType(document.getElementById("textDisplay"), `You attempted to defend against ${storyFight.monster.name}'s attack, and it blocked their attack, allowing for a counter attack.`);
+                Fight(storyFight)
             }
-            Fight(storyFight)
+
         }
         if(enemyChoice == 1){
-            textType(document.getElementById("textDisplay"), `You attempted to defend against ${storyFight.monster.name}'s attack, but they also attempted to block, leaving no progress to be made in the battle.`);
             if(player.health <= 0){
                 fightLose(storyFight)
-                return
-            }
-            if(storyFight.monster.health <= 0){
+                
+            }else if(storyFight.monster.health <= 0){
                 fightWin(storyFight)
-                return
+                
+            } else {
+                textType(document.getElementById("textDisplay"), `You attempted to defend against ${storyFight.monster.name}'s attack, but they also attempted to block, leaving no progress to be made in the battle.`);
+                Fight(storyFight)
             }
-            Fight(storyFight)
         }
         if(enemyChoice == 2){
-            textType(document.getElementById("textDisplay"), `You attempted to defend against ${storyFight.monster.name}'s attack, but they were waiting for your block and countered it without repercussion.`);
             player.health -= 1;
             if(player.health <= 0){
-                fightLose(storyFight)
-                return
+                fightLose(storyFight);
+                
+            }else if(storyFight.monster.health <= 0){
+                fightWin(storyFight);
+                
+            } else {
+                textType(document.getElementById("textDisplay"), `You attempted to defend against ${storyFight.monster.name}'s attack, but they were waiting for your block and countered it without repercussion.`);
+                Fight(storyFight);
             }
-            if(storyFight.monster.health <= 0){
-                fightWin(storyFight)
-                return
-            }
-            Fight(storyFight)
         }
         
     });
     document.getElementById(`wait`).addEventListener("click", () => {
         if(enemyChoice == 0){
-            textType(document.getElementById("textDisplay"), `You attempted to wait for ${storyFight.monster.name} to block so you could counterattack, but they just attacked, leading to you taking damage.`);
+            player.health -= 1;
             if(player.health <= 0){
                 fightLose(storyFight)
-                return
-            }
-            if(storyFight.monster.health <= 0){
+                
+            }else if(storyFight.monster.health <= 0){
                 fightWin(storyFight)
-                return
+                
+            } else {
+                textType(document.getElementById("textDisplay"), `You attempted to wait for ${storyFight.monster.name} to block so you could counterattack, but they just attacked, leading to you taking damage.`);
+                Fight(storyFight);
             }
-            Fight(storyFight)
         }
         if(enemyChoice == 1){
-            textType(document.getElementById("textDisplay"), `You attempted to wait for ${storyFight.monster.name} to block so you could counterattack, and it worked, leading to a successful attack on ${storyFight.monster.name}.`);
+            storyFight.monster.health -= player.attack;
             if(player.health <= 0){
                 fightLose(storyFight)
-                return
-            }
-            if(storyFight.monster.health <= 0){
+                
+            }else if(storyFight.monster.health <= 0){
                 fightWin(storyFight)
-                return
+                
+            } else {
+                textType(document.getElementById("textDisplay"), `You attempted to wait for ${storyFight.monster.name} to block so you could counterattack, and it worked, leading to a successful attack on ${storyFight.monster.name}.`);
+                Fight(storyFight)
             }
-            Fight(storyFight)
         }
         if(enemyChoice == 2){
-            textType(document.getElementById("textDisplay"), `You attempted to wait for ${storyFight.monster.name} to block so you could counterattack, but they had the same plan, leading to both of you awkwardly standing in place doing nothing.`);
             if(player.health <= 0){
                 fightLose(storyFight)
-                return
-            }
-            if(storyFight.monster.health <= 0){
+                
+            }else if(storyFight.monster.health <= 0){
                 fightWin(storyFight)
-                return
+                
+            } else {
+                textType(document.getElementById("textDisplay"), `You attempted to wait for ${storyFight.monster.name} to block so you could counterattack, but they had the same plan, leading to both of you awkwardly standing in place doing nothing.`);
+                Fight(storyFight);
             }
-            Fight(storyFight)
         }
         
     });
@@ -226,14 +230,18 @@ function Fight(storyFight){
 }
 
 function fightWin(storyFight){
-    document.getElementById("textDisplay").innerText = ``;
+    document.getElementById("textDisplay").textContent = ``;
     if(storyFight.monster.ending === 1){
+        document.getElementById("textDisplay").textContent = ``;
         moveTo(firstFightWin);
     } else if (storyFight.monster.ending === 2){
+        document.getElementById("textDisplay").textContent = ``;
         moveTo();
     } else if (storyFight.monster.ending === 3){
+        document.getElementById("textDisplay").textContent = ``;
         moveTo();
     } else {
+        document.getElementById("textDisplay").textContent = ``;
         if (player.losses === 2){
             moveTo();
         }
@@ -242,14 +250,20 @@ function fightWin(storyFight){
 }
 
 function fightLose(storyFight){
-    document.getElementById("textDisplay").innerText = ``;
+    document.getElementById("textDisplay").textContent = ``;
     if(storyFight.monster.ending === 1){
+        document.getElementById("textDisplay").textContent = ``;
+        player.health = 2;
         moveTo(secondRight);
     } else if (storyFight.monster.ending === 2){
+        document.getElementById("textDisplay").textContent = ``;
         moveTo();
     } else if (storyFight.monster.ending === 3){
+        document.getElementById("textDisplay").textContent = ``;
         moveTo();
     } else {
+        document.getElementById("textDisplay").textContent = ``;
+        player.health = 1;
         player.losses += 1
         if (player.losses === 2){
             moveTo();
